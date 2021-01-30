bdf901230f1893841302f2224be609ed01198fb1
Matt Manzi <manzi.matthewr@gmail.com>
2021-01-30 16:37:09 -0500

    add todos test todo finder

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,5 +1,5 @@
 // Package version holds version information about the project.
 package version
 
-// VersionString indicates the current semantic version of the project.
+// VersionString indicates the current semantic version of the project. tOdo: test todo finder
 const VersionString = "0.1.3"
