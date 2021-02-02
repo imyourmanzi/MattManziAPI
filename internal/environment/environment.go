34aab4c4934a72edf965118637729bd3db4b4884
Matt Manzi <manzi.matthewr@gmail.com>
2021-02-01 22:10:32 -0500

    add ability to read mongo uri from env (+ default)

--- internal/environment/environment.go
+++ internal/environment/environment.go
@@ -8,6 +8,10 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// EnvDatabaseURI is the name of the environment variable specifying the API's
+// database connection string.
+const EnvDatabaseURI = "MM_DATABASE_URI"
+
 // EnvMMEnvironment is the name of the environment variable for the API's
 // running environment.
 const EnvMMEnvironment = "MM_ENVIRONMENT"
@@ -15,6 +19,17 @@ const EnvMMEnvironment = "MM_ENVIRONMENT"
 // EnvVerbosity is the name of the environment variable indicating the API's
 // verbosity of logging.
 const EnvVerbosity = "MM_VERBOSITY"
+
+// DatabaseURI returns the connection string for the API's database, default
+// behavior is to connect to localhost.
+func DatabaseURI() string {
+	uri, set := os.LookupEnv(EnvDatabaseURI)
+	if !set {
+		uri = "mongodb://localhost:27017/mattmanzi_com?tls=true&tlsCertificateKeyFile=.local/x509/client.pem&tlsCAFile=.local/x509/ca.pem&authMechanism=MONGODB-X509&authSource=%24external&compressors=disabled"
+	}
+
+	return uri
+}
 
 // MMEnvironment returns the environment we're running in, but defaults to
 // "local".
